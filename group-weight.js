15a2f5ebb07f2c5d9ef8af1d3ff7eb0e79fa2b68
AlexiaC <alex.couton@gmail.com>
2018-10-15 16:41:08 +0200

    add weight compute way

---
+++ group-weight.js
@@ -0,0 +1,29 @@
+const R = require('ramda');
+
+const {nbMotsIdentiques_} = require('./wordpercent');
+
+const sentences = ['Je voudrais un ticket pour Lille',
+  'J aimerais un billet pour Toulouse',
+  'Un vol pour Amsterdam'];
+
+const maxWeight_ = R.pipe(
+  R.reduce(R.maxBy(R.prop(1)), ['', -Infinity]),
+  R.prop(1)
+);
+
+const computeWeight = (p, list) => [
+  p[0],
+  p[1],
+  Math.round((p[1] / maxWeight_(list)) * 100) * 0.01
+];
+const calcWeight = R.curry((list, p) => computeWeight(p, list));
+const mapP = list => R.map(calcWeight(list), list);
+
+const wordWeight_ = R.pipe(
+  nbMotsIdentiques_,
+  R.toPairs,
+  mapP,
+);
+
+console.log(sentences);
+console.log(wordWeight_(sentences));
